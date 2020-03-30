c5016e95597da6481a23b49e0a871e11c019a388
nurasyikin3104 <nur.asyikin@students.amikom.ac.id>
2020-03-30 22:54:05 +0700

    Add project files.

---
+++ TheCoffeMechine/CoffeMechine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCoffeMechine
+{
+    class CoffeMechine
+    {
+        private CoffePowder coffePowder;
+        private WaterGalon waterGalon;
+
+        public CoffeMechine(CoffePowder powder, WaterGalon galon)
+        {
+            this.waterGalon = galon;
+            this.coffePowder = powder;
+        }
+
+        public String makeEsspresso()
+        {
+            if (!this.waterGalon.isAvailable())
+            {
+                return "Sorry, the water is Empty";
+            }
+            if (!this.coffePowder.isAvailable())
+            {
+                return "Sorry, the coffe is empty";
+            }
+            this.waterGalon.makeOneCup();
+            this.coffePowder.makeOneCup();
+            return "Yey! your coffe is redy";
+        }
+
+        public String checkAvailability()
+        {
+            return $"The Water : {this.waterGalon.getVolume()} and the Coffe Powder powder : { this.coffePowder.getNetto()} ";
+        }
+    }
+}
---
+++ TheCoffeMechine/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TheCoffeMechine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CoffePowder coffe = new CoffePowder(100);
+            WaterGalon water = new WaterGalon(1000);
+            CoffeMechine yummycoffe = new CoffeMechine(coffe, water);
+
+            //1st cup coffe
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            String result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            //2nd cup coffe
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            //3nd cup coffe
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            //4nd cup coffe
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            //5nd cup coffe
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            //6nd cup coffe
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("Check " + yummycoffe.checkAvailability());
+        }
+    }
+}
